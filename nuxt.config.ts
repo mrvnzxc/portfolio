ddcc326d91c33232ae4a526ca5dc9a48ac2412af
John Marvin Bautista <johnmarvinbautista18@gmail.com>
2026-04-29 21:40:18 +0800

    try

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    '@vercel/analytics/nuxt'
+  ],
   css: ['~/assets/css/custom.css'],
   app: {
     head: {
